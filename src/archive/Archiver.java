1a2ebe545e972a92d028723137b58734c2fcd68a
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-18 20:21:03 +0300

    Added a method that asks the user about the operation, rewritten the main method

--- src/archive/Archiver.java
+++ src/archive/Archiver.java
@@ -1,11 +1,12 @@
 package archive;
 
-import command.ExitCommand;
-import java.nio.file.Paths;
+import exception.WrongZipFileException;
+
+import java.io.IOException;
 
 public class Archiver { //главный класс архиватор
     public static void main(String[] args) {
-        try{
+        /*try{
             ConsoleHelper.writeMessage("Введите полный путь архива:");
             String zipFile = ConsoleHelper.readString();
             ZipFileManager manager = new ZipFileManager(Paths.get(zipFile));
@@ -19,6 +20,34 @@ public class Archiver { //главный класс архиватор
         }
         catch (Exception ex){
             ex.printStackTrace();
+        }*/
+
+        Operation operation = null;
+        do{
+            try{
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            }
+            catch (WrongZipFileException ex){
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            }
+            catch (Exception ex){
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
         }
+        while (operation != Operation.EXIT);
+    }
+
+    public static Operation askOperation() throws IOException{ //узнать, какую команду хочет выполнить пользователь
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть сожержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+
+        int ordinal = ConsoleHelper.readInt();
+        return Operation.values()[ordinal];
     }
 }
