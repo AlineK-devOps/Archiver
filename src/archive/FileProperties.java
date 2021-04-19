524730ffc67a83e87d388e122cd687b887a15916
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-19 10:45:39 +0300

    Created a class that is responsible for the properties of files in the archive

---
+++ src/archive/FileProperties.java
@@ -0,0 +1,40 @@
+package archive;
+
+public class FileProperties { //свойства файлов в архиве
+    private String name; //имя файла
+    private long size; //размер файла в байтах
+    private long compressedSize; //размер после сжатия в байтах
+    private int compressionMethod; //метод сжатия
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod){
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public long getCompressionRatio(){ //считает степень сжатия
+        return 100 - (getCompressedSize() * 100 / getSize());
+    }
+
+    @Override
+    public String toString() {
+        return getSize() > 0? String.format("%s %d Kb (%d Kb) сжатие: %d%%", getName(), getSize() / 1024, getCompressedSize() / 1024, getCompressionRatio()) : getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+}
