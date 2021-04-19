103e2b6b6134929d5c1d50659fa699b14e243801
Aline Korshunova <aline.korshunova00@gmail.com>
2021-04-19 10:28:24 +0300

    Archive creation is implemented

--- src/command/ZipCommand.java
+++ src/command/ZipCommand.java
@@ -1,4 +1,14 @@
 package command;
 
+import archive.ConsoleHelper;
+import archive.ZipFileManager;
+
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{ // класс команд работющих непосредственно с архивом
+    public ZipFileManager getZipFileManager() throws Exception{ //создает менеджер архива
+        ConsoleHelper.writeMessage("Введите полный путь архива:");
+        String rootPath = ConsoleHelper.readString();
+        return new ZipFileManager(Paths.get(rootPath));
+    }
 }
--- src/command/ZipCreateCommand.java
+++ src/command/ZipCreateCommand.java
@@ -1,8 +1,24 @@
 package command;
 
+import archive.ConsoleHelper;
+import archive.ZipFileManager;
+import exception.PathIsNotFoundException;
+
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand{ //класс создания архива
     @Override
     public void execute() throws Exception {
-
+        try{
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла для архивации:");
+            String source = ConsoleHelper.readString();
+            zipFileManager.createZip(Paths.get(source));
+            ConsoleHelper.writeMessage("Архив создан");
+        }
+        catch (PathIsNotFoundException ex){
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
